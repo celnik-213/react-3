d30393556730e90f78f1e93a07712fa64dd9c3fa
Celnik-213 <uT_4000@zsp3zamosc.pl>
2026-05-12 10:15:05 +0200

    data zadanie

---
+++ src/components/DateTimeCard.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import '../App.css'
+
+function DateTime() {
+  const hours = new Date().getHours(); 
+  const minutes = new Date().getMinutes();
+  const day = new Date().getDate();
+  const month = new Date().getMonth() + 1; 
+  const year = new Date().getFullYear(); 
+  return ( 
+ 
+    <>
+    <p>Aktualna godzina: {hours}:{minutes}</p> 
+    <p>Data: {day}.{month}.{year}</p>
+  </>
+  
+  )
+}
+
+export default DateTime
